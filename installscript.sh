3700f040deef05c3f59554411580d3e84d04d263
David H <dhirsch1138@gmail.com>
2021-01-02 18:13:56 -0500

    add installer script

---
+++ installscript.sh
@@ -0,0 +1,39 @@
+echo installing compilation tool
+echo installing dpkg-dev
+apt-get -y install dpkg-dev
+echo installing git
+apt-get -y install git
+echo installing code libraries
+apt-get -y install libxrandr-dev
+apt-get -y install libxft-dev
+apt-get -y install libxinerama-dev
+apt-get -y install libx11-dev
+echo installing xorg
+apt-get -y install xorg
+echo install dwm and tools
+apt-get -y install dwm
+apt-get -y install suckless-tools
+apt-get -y install stterm
+echo install helpers
+apt-get -y install xautolock
+apt-get -y install nitrogen
+apt-get -y install trayer
+apt-get -y install dunst
+apt-get -y install pulseaudio
+apt-get -y install pnmixer
+echo install applications
+apt-get -y install tlp
+apt-get -y install gufw
+apt-get -y install vim
+apt-get -y install lxapperance
+apt-get -y install papirus-icon-theme
+apt-get -y install arc-theme
+apt-get -y install fonts-noto
+cd ~/Projects/suckless/dwm-6.2
+make install clean
+cd ~/Projects/suckless/dmenu-5.0
+make install clean
+cd ~/Projects/suckless/slock-1.4
+make install clean
+cd ~/Projects/suckless/st-0.8.4
+make install clean
