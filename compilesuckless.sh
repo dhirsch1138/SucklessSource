1af8a4a2e12a4ce46a1b8489b3c431057a1b7add
David H <dhirsch1138@gmail.com>
2021-01-03 21:08:59 -0500

    building install scripts

---
+++ compilesuckless.sh
@@ -0,0 +1,10 @@
+echo build suckless tools
+cd /home/david
+cd Projects/suckless/dwm-6.2
+make install clean
+cd ../dmenu-5.0
+make install clean
+cd ../slock-1.4
+make install clean
+cd ../st-0.8.4
+make install clean
---
+++ userscript.sh
@@ -0,0 +1,21 @@
+echo installing software, authenticate with "su" to escalate
+wget https://raw.githubusercontent.com/dhirsch1138/SucklessSource/master/installscript.sh installscript.sh
+chmod +x installscript.sh
+su --command="./installscript.sh"
+echo pulling down our suckless software git repo
+cd ~
+mkdir Projects
+cd Projects
+mkdir suckless
+git clone https://github.com/dhirsch1138/SucklessSource suckless
+echo compiling suckless tools, authenticate with "su" to escalate
+su --command="./compilesuckless.sh"
+echo building config files for this user
+touch ~/.xsessionrc
+echo "exec dwm" > ~/.xsessionrc
+cd ~
+mkdir .config
+cd .config
+mkdir pnmixer
+cd pnmixer
+cp ~/Projects/suckless/configdefaults/pnmixer/config config
